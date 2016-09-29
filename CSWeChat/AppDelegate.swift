9a4882d9c4fd2aac48f2164be99009b8884bfcef
yangxiaodong <fly_yxd@qq.com>
2016-09-29 16:11:04 +0800

    Cocoapods installed

--- CSWeChat/AppDelegate.swift
+++ CSWeChat/AppDelegate.swift
@@ -13,9 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window!.backgroundColor = UIColor.black
+        self.window!.rootViewController = MainTabBarController()
+        self.window!.makeKeyAndVisible()
+        
         return true
     }
 
---
+++ Podfile
@@ -0,0 +1,17 @@
+# platform :ios, '9.0'
+
+target 'CSWeChat' do
+  use_frameworks!
+
+  #Swift
+  pod 'Alamofire'
+  pod 'Kingfisher'
+  pod 'ObjectMapper'
+  pod 'SwiftyJSON'
+  pod 'XCGLogger'
+  pod 'BSImagePicker'
+
+  #Objective-C
+  pod 'SVProgressHUD'
+
+end
